661c8db62a6ae4dd65b239db3a803e01fef78b31
Jason <jbjbro6@gmail.com>
2025-03-18 22:00:56 -0400

    Removed errors on skinsdatatable.tsx

--- src/components/ui/skinsdatatable.tsx
+++ src/components/ui/skinsdatatable.tsx
@@ -8,36 +8,10 @@ import { ColumnDef, flexRender, getCoreRowModel, RowData, useReactTable } from "
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useEffect, useState } from "react";
 
-declare module "@tanstack/react-table" {
-  interface TableMeta<TData extends RowData, SkinsDataTable> {
-    updateData: (rowIndex: number, columnId: string, value: unknown) => void;
-  }
-}
-
 export interface SkinsDataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
 }
-
-const defaultColumn: Partial<ColumnDef<SkinsDataTable>> = {
-  cell: ({ getValue, row: { index }, column: { id }, table }) => {
-    const initialValue = getValue();
-    // We need to keep and update the state of the cell normally
-    const [value, setValue] = useState(initialValue);
-
-    // When the input is blurred, we'll call our table meta's updateData function
-    const onBlur = () => {
-      table.options.meta?.updateData(index, id, value);
-    };
-
-    // If the initialValue is changed external, sync it up with our state
-    useEffect(() => {
-      setValue(initialValue);
-    }, [initialValue]);
-
-    return id.includes("hole") || id.includes("out") || id.includes("in") || id.includes("adj") ? <input className="text-center w-8" value={value as string} onChange={(e) => setValue(e.target.value)} onBlur={onBlur} /> : <>{value}</>;
-  },
-};
 
 export function SkinsDataTable<TData, TValue>({ columns, data }: SkinsDataTableProps<TData, TValue>) {
   const [s, setS] = useState(data);
@@ -45,7 +19,6 @@ export function SkinsDataTable<TData, TValue>({ columns, data }: SkinsDataTableP
   const table = useReactTable({
     data,
     columns,
-    defaultColumn,
     getCoreRowModel: getCoreRowModel(),
     meta: {
       updateData: (rowIndex: number, columnId: any, value: any) => {
